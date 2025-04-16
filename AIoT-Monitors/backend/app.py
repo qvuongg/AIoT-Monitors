3db6771aaf5eb22c78f7b60559195a646dce362a
ldmpq <quyle83kd@gmail.com>
2025-04-16 21:42:00 +0700

    sigma boi

--- AIoT-Monitors/backend/app.py
+++ AIoT-Monitors/backend/app.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from app import create_app
+from app.routes.command_routes import command_bp
 
 # Load environment variables
 load_dotenv()
@@ -9,3 +10,6 @@ app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000))) 
+
+# backend/app/app.py
+app.register_blueprint(command_bp)
